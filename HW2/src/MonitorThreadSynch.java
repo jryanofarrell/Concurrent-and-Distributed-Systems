5a921c42197361fbf94bb2d11e66d8b0be80c0ed
jryanofarrell <jryanofarrell@utexas.edu>
2018-02-13 16:01:38 -0600

    finished problem 1 thread synch

--- HW2/src/MonitorThreadSynch.java
+++ HW2/src/MonitorThreadSynch.java
@@ -1,17 +1,43 @@
+import java.lang.Object;
+
 /*
  * EID's of group members
  * 
  */
 
 public class MonitorThreadSynch {
-	
+		
+	private int countdown;
+	private int countup;
+	private int parties;
 	public MonitorThreadSynch(int parties) {
+		this.parties = parties;
+		countdown = parties;
+		countup = 0; 
 	}
 	
-	public int await() throws InterruptedException {
-           int index = 0;
+	public synchronized int await() throws InterruptedException {
 		
-          // you need to write this code
+   		int index = 0;
+   		countdown--; 
+   		index = countdown;
+   		while(countup > 0){
+   			wait();
+   		}
+   		
+   		while(countdown>0){
+   			wait();
+   		}
+   		
+   		countup++;
+   		if(countup == parties){
+   			countup = 0;
+   			countdown = parties;
+   		}
+   		notifyAll(); 
+   		
+   		
+   		
 	    return index;
 	}
 }
--- HW2/src/ThreadSynch.java
+++ HW2/src/ThreadSynch.java
@@ -6,13 +6,54 @@ import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class ThreadSynch {
 	
+	private int parties;
+	private int countdown;
+	private Semaphore threadsynch;
+	
+	private Semaphore mutex; 
+	
+	private int countup; 
+	private Semaphore roundsynch;
+
 	public ThreadSynch(int parties) {
+		mutex = new Semaphore(1); 
+		threadsynch = new Semaphore(0);
+		this.parties = parties;
+		countdown = parties; 
+		countup = 0; 
+		
+		roundsynch = new Semaphore(0); 
+
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
 		
-          // you need to write this code
+
+		int index = 0; 
+		
+		mutex.acquire();
+		countdown--; 
+		index = countdown;
+		mutex.release();
+		while(countup > 0){
+			roundsynch.acquire();
+		}
+		while(countdown>0){
+			threadsynch.acquire();
+		}
+		
+		mutex.acquire();
+		countup++; 
+		threadsynch.release();
+		mutex.release();
+		if(countup == parties){
+			countup = 0;
+			countdown = parties;
+			roundsynch.release();
+		}
+		
+		
+		
 	    return index;
 	}
 }
