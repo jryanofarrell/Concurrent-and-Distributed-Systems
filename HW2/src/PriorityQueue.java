51acd18d0e353dd83c59478eba7b628c697751a4
jryanofarrell <jryanofarrell@utexas.edu>
2018-02-13 14:19:04 -0600

    added base files

---
+++ HW2/src/PriorityQueue.java
@@ -0,0 +1,28 @@
+
+public class PriorityQueue {
+
+	public PriorityQueue(int maxSize) {
+        // Creates a Priority queue with maximum allowed size as capacity
+	}
+
+	public int add(String name, int priority) {
+		return priority;
+        // Adds the name with its priority to this queue.
+        // Returns the current position in the list where the name was inserted;
+        // otherwise, returns -1 if the name is already present in the list.
+        // This method blocks when the list is full.
+	}
+
+	public int search(String name) {
+		return 0;
+        // Returns the position of the name in the list;
+        // otherwise, returns -1 if the name is not found.
+	}
+
+	public String getFirst() {
+		return null;
+        // Retrieves and removes the name with the highest priority in the list,
+        // or blocks the thread if the list is empty.
+	}
+}
+
