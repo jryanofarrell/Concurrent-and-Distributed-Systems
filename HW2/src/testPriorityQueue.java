120318373f1d40eee3213b7b2e174cdf59be8ec6
p-a-g-a <markcarter25@gmail.com>
2018-02-14 17:37:02 -0600

    changed tests

--- HW2/src/testPriorityQueue.java
+++ HW2/src/testPriorityQueue.java
@@ -43,12 +43,49 @@ public class testPriorityQueue implements Runnable {
 	public static void main(String[] args) {
 		Thread[] t = new Thread[SIZE];
 		PriorityQueue queue = new PriorityQueue(25);
+		Thread test1 = new Thread() {
+		      public void run(){
+		    	  int priority = ThreadLocalRandom.current().nextInt(0, 9 + 1);
+					String str = generateString();
+					System.out.println("Thread " + Thread.currentThread().getId() + " adding " + str + " priority " + priority);
+					try {
+						queue.add(str, priority);
+						//System.out.println(queue.search(str));
+						queue.print_list();
+						Thread.sleep(1000);
+					} catch(Exception e) {
+						e.printStackTrace();
+					}
+		        }
+		 };
+		Thread test2 = new Thread()  {
+		      public void run(){
+		    	  
+		    	  try {
+		    		  
+						System.out.println("Thread " + Thread.currentThread().getId() + " getting first " + queue.getFirst());
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}		        
+		      }
+		 };
+		 test1.start();
+		 try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 test2.start();
+		 /*
 		for (int i = 0; i < SIZE; ++i) {
 			t[i] = new Thread(new testPriorityQueue(queue));
 		}
 		for (int i = 0; i < SIZE; ++i) {
 			t[i].start();
 		}
+		*/
     }
 
 }
