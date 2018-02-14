3afd9d13484202e5b241850f14460239e956665a
jryanofarrell <jryanofarrell@utexas.edu>
2018-02-14 15:45:38 -0600

    fixed error

--- HW2/src/testPriorityQueue.java
+++ HW2/src/testPriorityQueue.java
@@ -1,10 +1,42 @@
 
 public class testPriorityQueue implements Runnable {
-
+	int thread_number; 
+	final static int SIZE = 2;
+	public testPriorityQueue(int thread_number){
+		this.thread_number = thread_number; 
+	}
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		switch(thread_number){
+			case 1: 
+				thread1_run();
+				break;
+				
+			case 2:
+				thread2_run();
+				break;
+		}
 		
 	}
-
+	
+	public void thread1_run(){
+		
+	}
+	
+	public void thread2_run(){
+		
+	}
+	
+	public static void main(){
+		PriorityQueue queue = new PriorityQueue(10); 
+		Thread[] t = new Thread[SIZE];
+		
+		for (int i = 0; i < SIZE; ++i) {
+			t[i] = new Thread(new testPriorityQueue(i+1));
+		}
+		
+		for (int i = 0; i < SIZE; ++i) {
+			t[i].start();
+		}
+    }
 }
