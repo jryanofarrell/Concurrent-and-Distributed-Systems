c11e2d6ddc5d62d091538257cbeec1830e3cea39
p-a-g-a <markcarter25@gmail.com>
2018-03-05 16:13:59 -0600

    add record class

---
+++ HW3/src/Record.java
@@ -0,0 +1,11 @@
+public class Record {
+	String studentName;
+	String bookName;
+	int recordID;
+
+	public Record(String s, String b, int id) {
+		studentName = s;
+		bookName = b;
+		recordID = id;
+	}
+}
