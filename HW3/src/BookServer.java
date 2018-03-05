cdb46565b29fc2a3c7256f1f47ff1d2422bdfa92
p-a-g-a <markcarter25@gmail.com>
2018-03-05 11:04:55 -0600

    Parsed inventory file and stored in HashMap

--- HW3/src/BookServer.java
+++ HW3/src/BookServer.java
@@ -1,4 +1,8 @@
+import java.util.HashMap;
+import java.io.*;
+
 public class BookServer {
+  private static HashMap<String, Integer> inventory = new HashMap<String, Integer>();
   public static void main (String[] args) {
     int tcpPort;
     int udpPort;
@@ -11,7 +15,25 @@ public class BookServer {
     udpPort = 8000;
 
     // parse the inventory file
+    try {
+      FileReader input = new FileReader(args[0]);
+      BufferedReader bufRead = new BufferedReader(input);
+      String myLine = null;
 
+      while ((myLine = bufRead.readLine()) != null) {   
+          String[] lineArray = myLine.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+          System.out.println(lineArray[0] + " " + lineArray[1]);
+          String bookTitle = lineArray[0];
+          Integer bookQuantity = Integer.valueOf(lineArray[1]);
+          inventory.put(bookTitle, bookQuantity);
+      }
+    } catch(Exception e) {
+      System.out.println("Error while parsing inventory file.");
+      e.printStackTrace();
+    }
+    for(String k : inventory.keySet())
+      System.out.println(k + " " + inventory.get(k));
     // TODO: handle request from clients
+    
   }
 }
